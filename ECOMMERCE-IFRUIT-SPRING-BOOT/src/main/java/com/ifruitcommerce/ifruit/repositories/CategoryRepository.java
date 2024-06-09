5ff5a783155a2a3cdd9f4277e777b515422ff522
CarlosMocinho <140860836+CarlosMocinho@users.noreply.github.com>
2024-06-09 11:15:10 -0300

    Delete ECOMMERCE-IFRUIT-SPRING-BOOT directory

--- ECOMMERCE-IFRUIT-SPRING-BOOT/src/main/java/com/ifruitcommerce/ifruit/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ifruitcommerce.ifruit.repositories;
-
-import com.ifruitcommerce.ifruit.entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long>{
-}
--- ECOMMERCE-IFRUIT-SPRING-BOOT/src/main/java/com/ifruitcommerce/ifruit/repositories/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ifruitcommerce.ifruit.repositories;
-
-import com.ifruitcommerce.ifruit.entities.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
-}
